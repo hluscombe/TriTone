caae416e5498fd6227f31d2e052c848b31d4c661
Hugh Luscombe <hughluscombe@gmail.com>
2019-06-12 15:11:31 +1000

    testing hash-router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import Animations from './components/Animations.js'
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 // import Sounds from './components/Sounds';
 
 function App() {
   return (
     <div className="App">
-      <Animations />
+      <BrowserRouter basename="/tritone-index">
+        <Switch>
+          <Route exact path='/' component={Animations}></Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   )
 }
 
-export default App;
 
-// <Sounds />
+
+
+export default App;
